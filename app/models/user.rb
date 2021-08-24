fc801ca6e4b0f7ed8fb9a5b3f216ef5ed6728839
mathis etournay <mathis@treno.fr>
2021-08-24 17:56:12 +0200

    changements categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
   has_many :donation_sessions
   has_many :donations, through: :donation_sessions
+
+  validate :favorite_sub_categories_is_in_list
+
+  private
+
+  def favorite_sub_categories_is_in_list
+    if favorite_sub_categories.any? {|sub_category| !Nonprofit::SUB_CATEGORIES.include?(sub_category)}
+      errors.add :favorite_sub_categories, :inclusion
+    end
+  end
 end
