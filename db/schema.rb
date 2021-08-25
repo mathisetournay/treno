e342b342c74310fcab137647126b4a27df1b936f
mathis etournay <mathis@treno.fr>
2021-08-25 15:04:25 +0200

    migration user + génération seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_135606) do
+ActiveRecord::Schema.define(version: 2021_08_25_125918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "donation_sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -54,10 +75,15 @@ ActiveRecord::Schema.define(version: 2021_08_24_135606) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "favorite_sub_categories", default: [], array: true
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.integer "donation_budget"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "donation_sessions", "users"
   add_foreign_key "donations", "donation_sessions"
   add_foreign_key "donations", "nonprofits"
