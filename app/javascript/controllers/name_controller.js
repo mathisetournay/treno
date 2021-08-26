cee563ca14f743156d413a595c1c305a9dc2b1e2
Flore Sauloup <flore.sauloup@audencia.com>
2021-08-26 18:52:12 +0200

    name js define

---
+++ app/javascript/controllers/name_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["firstNameInput", "firstName"]
+
+  setFirstName(event) {
+    event.preventDefault()
+    const firstName = this.firstNameInputTarget.value
+
+    this.firstNameTargets.forEach(firstNameTarget => {
+      firstNameTarget.innerHTML = firstName
+    });
+  }
+}
