e409d3c3a8e11887d2691ad8f1d8b4f3c85a7ef7
Nans Petit <nans.petit@audencia.com>
2021-08-30 13:18:14 +0200

    date good dashboard

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,4 +3,8 @@ class Donation < ApplicationRecord
   belongs_to :donation_session
 
   has_one :user, through: :donation_session
+
+  def display_date
+    created_at.strftime("%m/%d/%Y at %I:%M%p")
+  end
 end
