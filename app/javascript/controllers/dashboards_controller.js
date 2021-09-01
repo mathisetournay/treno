3b4b104c2158179d0276d23244f70d1b024ddfae
mathis etournay <mathis@treno.fr>
2021-09-01 09:51:18 +0200

    fix js

--- app/javascript/controllers/dashboards_controller.js
+++ app/javascript/controllers/dashboards_controller.js
@@ -3,32 +3,32 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["firstlogo", "secondlogo"]
 
-  const menu = document.querySelector(`#menu`);
+//   const menu = document.querySelector(`#menu`);
 
-  // set each style property separately
-  menu.style.borderRadius = '4em';
+//   // set each style property separately
+//   menu.style.borderRadius = '4em';
 
-  menu.style.border = '1px solid red';
+//   menu.style.border = '1px solid red';
+// // menu.style.border, equals to
+
+//   /*
+//   menu.style.borderWidth = '1px';
+//   menu.style.borderStyle = 'solid';
+//   menu.style.borderColor = 'red';
+
+//   */const menu = document.querySelector(`#menu`);
+
+//   // set each style property separately
+//   menu.style.borderRadius = '4em';
+
+//   menu.style.border = '1px solid red';
 // menu.style.border, equals to
 
   /*
   menu.style.borderWidth = '1px';
   menu.style.borderStyle = 'solid';
   menu.style.borderColor = 'red';
-  
-  */const menu = document.querySelector(`#menu`);
 
-  // set each style property separately
-  menu.style.borderRadius = '4em';
-
-  menu.style.border = '1px solid red';
-// menu.style.border, equals to
-
-  /*
-  menu.style.borderWidth = '1px';
-  menu.style.borderStyle = 'solid';
-  menu.style.borderColor = 'red';
-  
   */
 
   setFirstName(event) {
